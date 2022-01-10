8725da07dc1aff1afe77faf0cc1a6a2dbee8be15
luirusso <luirusso_@hotmail.it>
2022-01-10 14:55:15 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,4 @@
+<!-- Descrizione
+Oggi pomeriggio provate ad immaginare alcune classi necessarie per creare uno shop online; ad esempio, ci saranno sicuramente dei prodotti da acquistare e degli utenti che fanno shopping.
+Strutturare le classi gestendo l’ereditarietà dove necessario; ad esempio ci potrebbero essere degli utenti premium che hanno diritto a degli sconti esclusivi, oppure diverse tipologie di prodotti.
+Eseguire poi degli output istanziando oggetti delle varie classi. -->
